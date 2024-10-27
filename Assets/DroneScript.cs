9e7ea8ea4ad9ec9f137064cea07079b6748fbd92
tyoma-ka <kaliadin1@uniba.sk>
2024-10-27 19:31:01 +0100

    create project

---
+++ Assets/DroneScript.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneScript : MonoBehaviour
+{
+    public List<Vector3> positions = new List<Vector3>
+{
+    new Vector3(0, 0, 0),
+    new Vector3(2, 0, 1),
+    new Vector3(4, 0, 2),
+    new Vector3(6, 0, 0),
+    new Vector3(8, 1, -1),
+    new Vector3(10, 0, 0)
+};
+    private int currentPositionIndex = 0;
+
+    public float speed = 1.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (positions.Count > 0 && currentPositionIndex < positions.Count)
+        {
+            // Move the object towards the next position
+            transform.position = Vector3.MoveTowards(transform.position, positions[currentPositionIndex], speed * Time.deltaTime);
+
+            // Check if the object has reached the current target position
+            if (Vector3.Distance(transform.position, positions[currentPositionIndex]) < 0.01f)
+            {
+                currentPositionIndex++;
+            }
+        }
+    }
+}

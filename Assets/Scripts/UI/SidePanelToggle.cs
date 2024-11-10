f3915549b9fa36cee13929d6d986a5b032524927
Ondrej Babinský <babinskyondrej@gmail.com>
2024-11-10 20:23:13 +0100

    SidePanel and Upload File Script addition
    Pridal som StandaloneFileBrowser import, ktory pomaha s pracou s files.
    Vytvoril som side panel, ktory sa da schovat pomocou buttonu a skriptu SidePanelToggle.cs.
    Tiez sa v nom da vlozit drone command file pomocou StandaloneFileBrowser  a scriptu FileUploadController.cs.

---
+++ Assets/Scripts/UI/SidePanelToggle.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SidePanelToggle : MonoBehaviour
+{
+    private VisualElement sidePanel;
+    private Button toggleButton;
+    private bool isPanelVisible = true;
+
+    void OnEnable()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        sidePanel = root.Q<VisualElement>("side-panel");
+        toggleButton = root.Q<Button>("toggle-panel-button");
+
+        toggleButton.clicked += TogglePanel;
+
+        UpdatePanelState();
+    }
+
+    void TogglePanel()
+    {
+        isPanelVisible = !isPanelVisible;
+        UpdatePanelState();
+    }
+
+    void UpdatePanelState()
+    {
+        if (isPanelVisible)
+        {
+            sidePanel.RemoveFromClassList("hidden");
+            toggleButton.style.left = 400;
+            toggleButton.text = "<";
+        }
+        else
+        {
+            sidePanel.AddToClassList("hidden");
+            toggleButton.style.left = 0;
+            toggleButton.text = ">";
+        }
+    }
+}

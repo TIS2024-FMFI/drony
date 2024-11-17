885fe6b2f412566ea3d2ecbac4f430b083414b6a
Ondrej Babinský <66784901+lexendo@users.noreply.github.com>
2024-11-17 01:09:23 +0100

    FileUploadController.cs
    Implementovaná funkcionalita na nahrávanie súborov v SidePaneli po kliknuti buttonu Upload Command File pomocou skriptu FileUploadController.cs

---
+++ Assets/Scripts/FileImporter/FileUploadController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+using System.IO;
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR
+using SFB;
+#endif
+
+public class FileUploadController : MonoBehaviour
+{
+    private Button uploadButton;
+    private Label fileNameLabel;
+    private string filePath;
+
+    void OnEnable()
+    {
+        // root element z UIDocument
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        uploadButton = root.Q<Button>("upload-button");
+        fileNameLabel = root.Q<Label>("file-name-label");
+
+        // listener na tlacidlo
+        uploadButton.clicked += OnUploadButtonClick;
+    }
+
+    private void OnUploadButtonClick()
+    {
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR
+        var extensions = new[] { new ExtensionFilter("Text Files", "txt") };
+        string[] paths = StandaloneFileBrowser.OpenFilePanel("Select a file", "", extensions, false);
+
+        if (paths.Length > 0 && !string.IsNullOrEmpty(paths[0]))
+        {
+            filePath = paths[0];
+            fileNameLabel.text = Path.GetFileName(filePath);
+            ProcessFile(filePath);
+        }
+#endif
+    }
+
+    private void ProcessFile(string path)
+    {
+        // tu ten file procesneme, config cast spracuje nejaky configcontroller a ostatok spracuje parser a interpreter pre commandy
+        string fileContent = File.ReadAllText(path);
+        Debug.Log("Loaded File Content: " + fileContent);
+
+    }
+}

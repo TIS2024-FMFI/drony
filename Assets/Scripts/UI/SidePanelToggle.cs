37b7b194653ed2b46dbf17aad4db732888a374cb
Ondrej Babinský <babinskyondrej@gmail.com>
2024-12-10 01:30:18 +0100

    sidepanel width from 300px to 400px
    sidepanel width from 300px to 400px
    it was hardcoded 300 now its 400

--- Assets/Scripts/UI/SidePanelToggle.cs
+++ Assets/Scripts/UI/SidePanelToggle.cs
@@ -30,7 +30,7 @@ public class SidePanelToggle : MonoBehaviour
         if (isPanelVisible)
         {
             sidePanel.RemoveFromClassList("hidden");
-            toggleButton.style.left = 300;
+            toggleButton.style.left = 400;
             toggleButton.text = "<";
         }
         else
